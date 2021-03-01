fba2edfb6c1a7dfeae773321af4481575a5d636d
biutas@hotmail.com <biutas@hotmail.com>
2021-03-01 20:27:02 -0300

    Fix(module): updated type on module.

--- src/lib/bootstrap-icons/ng-bootstrap-icons.module.ts
+++ src/lib/bootstrap-icons/ng-bootstrap-icons.module.ts
@@ -1,34 +1,27 @@
-import { NgModule, ModuleWithProviders, Optional } from '@angular/core';
-import { BootstrapIcons } from './ng-bootstrap-icons.component';
-import { Icons } from './icons.provider';
-
+import { NgModule, ModuleWithProviders, Optional } from "@angular/core";
+import { BootstrapIcons } from "./ng-bootstrap-icons.component";
+import { Icons } from "./icons.provider";
 
 @NgModule({
-  declarations: [
-    BootstrapIcons
-  ],
-  exports: [
-    BootstrapIcons
-  ]
+  declarations: [BootstrapIcons],
+  exports: [BootstrapIcons],
 })
 export class BootstrapIconsModule {
-  constructor(
-    @Optional() private icons: Icons
-  ) {
-    if ( !this.icons ) {
+  constructor(@Optional() private icons: Icons) {
+    if (!this.icons) {
       throw new Error(
         `No icon provided. Make sure to use 'BootstrapIconsModule.pick({ ... })' when importing the module\n` +
-        `Refer to documentation on https://github.com/biutas/ng-bootstrap-icons/issues`
+          `Refer to documentation on https://github.com/biutas/ng-bootstrap-icons/issues`
       );
     }
   }
 
-  static pick(icons: {[key: string]: string}): ModuleWithProviders {
+  static pick(icons: {
+    [key: string]: string;
+  }): ModuleWithProviders<BootstrapIconsModule> {
     return {
       ngModule: BootstrapIconsModule,
-      providers: [
-        { provide: Icons, multi: true, useValue: icons }
-      ]
+      providers: [{ provide: Icons, multi: true, useValue: icons }],
     };
   }
 }
